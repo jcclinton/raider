688d8341bbad606394c0427942dd4d83301d38f0
unknown <Jamie Clinton@.(none)>
2010-10-06 22:04:54 -0700

    cleaned up client side code but did not add any new functionality.

--- client/client.php
+++ client/client.php
@@ -11,130 +11,24 @@ $(document).ready(function() {
 	}else{
 		//The user has WebSockets
 		
-		
-    function draw() {  
-      var canvas = document.getElementById("canvas");  
-      if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-		  
-		  var img = new Image();  
-		 img.onload = function(){  
-			ctx.drawImage(img,200,200);  
-		 }  
-		  img.src = 'zoqfot.big.12.png';
-		  
-      canvas.onmousedown = function(e) {
-        var mx = e.clientX;
-        var my = e.clientY;
-        ctx.drawImage(img, mx, my);
-				$('#chatLog').append('clicked at mx:'+mx+' my: '+my+'<br/>');
-      }
-		  
-			
-  
-       /* ctx.fillStyle = "rgb(200,0,0)";  
-        ctx.fillRect (10, 10, 55, 50);  
-  
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";  
-        ctx.fillRect (30, 30, 55, 50);  */
-      }  
-    } 
-	 
-	 $('#canvas').append('<img src="zoqfot.big.12.png" id = "sprite" style="position:absolute; top:200px; left:200px;" />');
-	
-	//draw();
-	connect();
-		
-	function connect(){
-			var socket;
-			var host = "ws://localhost:8000/socket/server/startDaemon.php";
-			
-			try{
-				var socket = new WebSocket(host);
-				message('<p class="event">Socket Status: '+socket.readyState+'</p>');
-				socket.onopen = function(){
-					message('<p class="event">Socket Status: '+socket.readyState+' (open)'+'</p>');	
-				}
-				
-				socket.onmessage = receive;
-				
-				socket.onclose = function(){
-					message('<p class="event">Socket Status: '+socket.readyState+' (Closed)'+'</p>');
-				}			
-					
-			} catch(exception){
-				message('<p>Error'+exception+'</p>');
+		function loadjscssfile(filename, filetype){
+			if (filetype=="js"){ //if filename is a external JavaScript file
+				var fileref=document.createElement('script')
+				fileref.setAttribute("type","text/javascript")
+				fileref.setAttribute("src", filename)
 			}
-			
-			function receive(msg){
-				message(msg.data, 1);
-				msg = "msg = "+msg.data;
-				eval(msg);
-				var speed;
-				if(msg.text == 'moving'){
-					speed = 10;
-					$('#sprite').animate({
-						top: msg.y,
-						left: msg.x
-					}, speed);
-					$('#chatLog').append(msg.x + ', '+  msg.y);	
-				}else{
-					msg = msg.text;
-				}
-			}
-				
-			function send(){
-				var text = $('#text').val();
-				if(text==""){
-					message('<p class="warning">Please enter a message'+'</p>');
-					return ;	
-				}
-				try{
-					socket.send(text);
-					message('<p class="event">Sent: '+text+'</p>')
-				} catch(exception){
-					message('<p class="warning">'+'</p>');
-				}
-				$('#text').val("");
-			}
-			
-			function message(msg, type){
-				if(type == 1){
-					msg = "msg = "+msg;
-					eval(msg);
-					msg = msg.text;
-					msg = '<p class="message">Received: ' + msg + '</p>';
-				}
-				$('#chatLog').append(msg);
-			}//End message()
-			
-			$('#text').keypress(function(event) {
-					  if (event.keyCode == '13') {
-						 send();
-					   }
-			});
-			
-			$('#text').click(function() {
-				//socket.send("{'action':'yo', 'x':150, 'y':150}");
-			});
-			
-			$('#canvas').click(function(e) {
-				var x = e.pageX - this.offsetLeft;
-				var y = e.pageY - this.offsetTop;
-				
-				//$('#chatLog').append(x +', '+ y + '<br/>');
-				socket.send('{"action":"move", "x":'+x+', "y":'+y+'}');
-			});
-			
-			$('#disconnect').click(function(){
-				socket.close();
-			});
+			if (typeof fileref!="undefined")
+				document.getElementsByTagName("head")[0].appendChild(fileref)
+		}
 
-		}		
+		loadjscssfile("socket.js", "js") //dynamically load and add this .js file
 		
+		$('#canvas').append('<img src="zoqfot.big.12.png" id = "sprite" style="position:absolute; top:200px; left:200px;" />');
 		
-	}//End connect()
+		//draw();
+		connect();	
 		
+	}
 });
 </script>
 <meta charset=utf-8 />
---
+++ client/socket.js
@@ -0,0 +1,120 @@
+
+		
+		
+    function draw() {  
+      var canvas = document.getElementById("canvas");  
+      if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+		  
+		  var img = new Image();  
+		 img.onload = function(){  
+			ctx.drawImage(img,200,200);  
+		 }  
+		  img.src = 'zoqfot.big.12.png';
+		  
+      canvas.onmousedown = function(e) {
+        var mx = e.clientX;
+        var my = e.clientY;
+        ctx.drawImage(img, mx, my);
+				$('#chatLog').append('clicked at mx:'+mx+' my: '+my+'<br/>');
+      }
+		  
+			
+  
+       /* ctx.fillStyle = "rgb(200,0,0)";  
+        ctx.fillRect (10, 10, 55, 50);  
+  
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";  
+        ctx.fillRect (30, 30, 55, 50);  */
+      }  
+    } 
+	 
+	 
+	
+		
+	function connect(){
+			var socket;
+			var host = "ws://localhost:8000/socket/server/startDaemon.php";
+			
+			try{
+				var socket = new WebSocket(host);
+				message('<p class="event">Socket Status: '+socket.readyState+'</p>');
+				socket.onopen = function(){
+					message('<p class="event">Socket Status: '+socket.readyState+' (open)'+'</p>');	
+				}
+				
+				socket.onmessage = receive;
+				
+				socket.onclose = function(){
+					message('<p class="event">Socket Status: '+socket.readyState+' (Closed)'+'</p>');
+				}			
+					
+			} catch(exception){
+				message('<p>Error'+exception+'</p>');
+			}
+			
+			function receive(msg){
+				message(msg.data, 1);
+				msg = "msg = "+msg.data;
+				eval(msg);
+				var speed;
+				if(msg.text == 'moving'){
+					speed = 10;
+					$('#sprite').animate({
+						top: msg.y,
+						left: msg.x
+					}, speed);
+					$('#chatLog').append(msg.x + ', '+  msg.y);	
+				}else{
+					msg = msg.text;
+				}
+			}
+				
+			function send(){
+				var text = $('#text').val();
+				if(text==""){
+					message('<p class="warning">Please enter a message'+'</p>');
+					return ;	
+				}
+				try{
+					socket.send(text);
+					message('<p class="event">Sent: '+text+'</p>')
+				} catch(exception){
+					message('<p class="warning">'+'</p>');
+				}
+				$('#text').val("");
+			}
+			
+			function message(msg, type){
+				if(type == 1){
+					msg = "msg = "+msg;
+					eval(msg);
+					msg = msg.text;
+					msg = '<p class="message">Received: ' + msg + '</p>';
+				}
+				$('#chatLog').append(msg);
+			}//End message()
+			
+			$('#text').keypress(function(event) {
+					  if (event.keyCode == '13') {
+						 send();
+					   }
+			});
+			
+			$('#text').click(function() {
+				//socket.send("{'action':'yo', 'x':150, 'y':150}");
+			});
+			
+			$('#canvas').click(function(e) {
+				var x = e.pageX - this.offsetLeft;
+				var y = e.pageY - this.offsetTop;
+				
+				//$('#chatLog').append(x +', '+ y + '<br/>');
+				socket.send('{"action":"move", "x":'+x+', "y":'+y+'}');
+			});
+			
+			$('#disconnect').click(function(){
+				socket.close();
+			});
+
+		}		
