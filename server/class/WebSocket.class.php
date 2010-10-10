664012d44cb64a9689326b1450656a9dd9ba8f9d
unknown <Jamie Clinton@.(none)>
2010-10-09 19:13:26 -0700

    removed some unused code

--- server/class/WebSocket.class.php
+++ server/class/WebSocket.class.php
@@ -12,52 +12,12 @@ class WebSocket extends socket
 	private $clients = array();
 	private $handshakes = array();
 
-	//private $unit = array();
-	private $queue = array();
-
-	//private $dt = 0;
-
 	public function __construct()
 	{
 		parent::__construct();
 
-		//$this->run();
 	}
 
-	/**
-	 * Runs the while loop, wait for connections and handle them
-	 */
-	/*private function run()
-	{
-		$time_end = microtime(true);
-		$this->init();
-		while(true)
-		{
-			$time_start = microtime(true);
-			$this->dt = $time_start - $time_end;
-			$time_end = $time_start;
-
-			$this->getInput();
-			$this->update();
-			$this->sendResponse();
-			if($this->dt < .1){
-				usleep(10000);
-			}
-		}// end game loop
-	}*/
-
-	/*private function init(){
-		$unit = array();
-		$unit['speed'] = 250; //pixels per second
-		$unit['x'] = 200;
-		$unit['y'] = 200;
-		$unit['dx'] = null;
-		$unit['dy'] = null;
-		$unit['status'] = 'stopped';
-		$this->unit = $unit;
-
-		$this->queue = array();
-	}*/
 
 	public function getInput(){
 
@@ -135,21 +95,6 @@ class WebSocket extends socket
 		return $queue;
 	}
 
-	/*private function update(){
-		if(!empty($this->queue)){
-			foreach($this->queue as $action){
-				$data = json_decode($action, true);
-				if($data['action'] == 'move' && isset($data['x']) && isset($data['y'])){
-					$this->unit['dx'] = $data['x'];
-					$this->unit['dy'] = $data['y'];
-					$this->console('currently at: '.$this->unit['x'].','.$this->unit['y'].'. going to '.$this->unit['dx'] . ', '.$this->unit['dy']);
-				}
-			}
-			$this->queue = array();
-		}
-
-		$this->updatePosition();
-	}*/
 
 	public function sendResponse($msg){
 		//$msg = array('response' => 'sucess', 'text' => 'moving', 'x'=>$this->unit['x'], 'y'=>$this->unit['y']);
@@ -163,72 +108,10 @@ class WebSocket extends socket
 		}
 	}
 
-	/*private function updatePosition(){
-		if(!is_null($this->unit['dx'] ) || !is_null($this->unit['dy'])){
-			$diffx = $this->unit['dx'] - $this->unit['x'];
-			$diffy = $this->unit['dy'] - $this->unit['y'];
-					$this->console('diffs: '.$diffx . ', '.$diffy);
-
-			if(($diffx == 0 && $diffy == 0) || ($this->unit['x'] < 20 || $this->unit['y'] < 20 || $this->unit['x'] > 380 || $this->unit['y'] > 380)){
-				$this->unit['dx'] = null;
-				$this->unit['dy'] = null;
-				$this->console('not moving');
-			}else if(abs($diffx) < 1 && abs($diffy) < 1){
-				$this->unit['x'] = $this->unit['dx'];
-				$this->unit['y'] = $this->unit['dy'];
-				$this->console('snapping shut');
-			}else{
-				$denom = sqrt(pow($diffx,2) + pow($diffy,2));
-				$rads = asin($diffy/$denom);
-
-					$this->console('currently at: '.$this->unit['x'].','.$this->unit['y']);
-				$this->unit['x'] += $this->unit['speed'] * $this->dt * cos($rads)*(-1);
-				$this->unit['y'] += $this->unit['speed'] * $this->dt * sin($rads);
-				$this->console('moving with speed: '.$this->unit['speed'] . ' and dt: '.$this->dt . ' and cos,sin: '.cos($rads) . ', '. sin($rads) . ' and denom/rads: ' . $denom . ', ' . $rads );
-					$this->console('now at: '.$this->unit['x'].','.$this->unit['y']);
-			}
-
-			if($this->unit['status'] != 'moving'){
-				$this->unit['status'] = 'moving';
-			}
-		}else{
-			if($this->unit['status'] == 'moving'){
-				$this->unit['status'] = 'stopped';
-			}
-		}
-	}*/
-
-	/**
-	 * Manage the handshake procedure
-	 *
-	 * @param string $buffer The received stream to init the handshake
-	 * @param socket $socket The socket from which the data came
-	 * @param int $socket_index The socket index in the allsockets array
-	 */
-	/*private function do_handshake($buffer,$socket,$socket_index)
-	{
-		$this->console('Requesting handshake...');
-
-		list($resource,$host,$origin) = $this->getheaders($buffer);
-
-		$this->console('Handshaking...');
-
-		$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-				"Upgrade: WebSocket\r\n" .
-				"Connection: Upgrade\r\n" .
-				"WebSocket-Origin: {$origin}\r\n" .
-				"WebSocket-Location: ws://{$host}{$resource}\r\n\r\n" . chr(0);
-
-		$this->handshakes[$socket_index] = true;
-
-		socket_write($socket,$upgrade,strlen($upgrade));
-
-		$this->console('Done handshaking...');
-	}*/
 
 
 
-	    public function doHandshake($buffer, $socket, $socket_index) {
+	public function doHandshake($buffer, $socket, $socket_index) {
 
         list($resource, $headers, $securityCode) = $this->handleRequestHeader($buffer);
 
